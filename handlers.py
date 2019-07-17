b2ff65601544ddd3d599042b9936330376ab5721
Vladimir <x55aah@gmail.com>
2019-07-17 11:47:12 +0300

    updates

--- handlers.py
+++ handlers.py
@@ -1,6 +1,7 @@
 from anthill.framework.handlers.streaming.uploadfile import UploadFileStreamHandler
+from anthill.platform.handlers import UserHandlerMixin
 
 
 # noinspection PyAbstractClass
-class DeployHandler(UploadFileStreamHandler):
+class DeployHandler(UploadFileStreamHandler, UserHandlerMixin):
     pass
